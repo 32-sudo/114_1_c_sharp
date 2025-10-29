1c294e58f101d8ce733679d28efdfd287f884dfb
32-sudo <a114221034@mail.shu.edu.tw>
2025-10-29 14:57:15 +0800

    114

---
+++ 114-10-29補/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.orangepicturebox = new System.Windows.Forms.PictureBox();
+            this.bananabox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orangepicturebox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bananabox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1049, 43);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(533, 439);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1020, 575);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(628, 542);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // orangepicturebox
+            // 
+            this.orangepicturebox.Image = ((System.Drawing.Image)(resources.GetObject("orangepicturebox.Image")));
+            this.orangepicturebox.Location = new System.Drawing.Point(99, 593);
+            this.orangepicturebox.Margin = new System.Windows.Forms.Padding(7);
+            this.orangepicturebox.Name = "orangepicturebox";
+            this.orangepicturebox.Size = new System.Drawing.Size(703, 542);
+            this.orangepicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.orangepicturebox.TabIndex = 2;
+            this.orangepicturebox.TabStop = false;
+            this.orangepicturebox.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // bananabox
+            // 
+            this.bananabox.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.bananabox.ErrorImage = null;
+            this.bananabox.Image = ((System.Drawing.Image)(resources.GetObject("bananabox.Image")));
+            this.bananabox.InitialImage = ((System.Drawing.Image)(resources.GetObject("bananabox.InitialImage")));
+            this.bananabox.Location = new System.Drawing.Point(86, 27);
+            this.bananabox.Margin = new System.Windows.Forms.Padding(7);
+            this.bananabox.Name = "bananabox";
+            this.bananabox.Size = new System.Drawing.Size(716, 478);
+            this.bananabox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bananabox.TabIndex = 3;
+            this.bananabox.TabStop = false;
+            this.bananabox.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(1820, 177);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(636, 144);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "總卡路里";
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Yellow;
+            this.textBox1.Location = new System.Drawing.Point(1822, 405);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(662, 55);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1856, 736);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(524, 128);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "重設";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1856, 931);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(518, 123);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "離開";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 40F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2583, 1531);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bananabox);
+            this.Controls.Add(this.orangepicturebox);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orangepicturebox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bananabox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox orangepicturebox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.PictureBox bananabox;
+    }
+}
+
